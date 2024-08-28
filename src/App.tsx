384f7063d8e87672d13b63c22036319a21588526
DoubleA <ruben.aadland@gmail.com>
2024-08-28 08:43:46 +0200

    Added Vercel analytics and speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Desktop from "./components/Desktop.tsx";
 import Taskbar from "./components/Taskbar.tsx";
+import {SpeedInsights} from "@vercel/speed-insights/react";
+import {Analytics} from "@vercel/analytics/react";
 
 function App() {
 
     return (
-        <div className={"app"}>
-            <Desktop/>
-            <Taskbar/>
-        </div>
+        <>
+            <div className={"app"}>
+                <Desktop/>
+                <Taskbar/>
+            </div>
+            <SpeedInsights/>
+            <Analytics/>
+        </>
     )
 }
 

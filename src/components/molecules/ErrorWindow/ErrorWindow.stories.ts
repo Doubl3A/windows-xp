73e32a44d35b60d1f105aa47f62e0226c9b993d5
DoubleA <ruben.aadland@gmail.com>
2024-09-18 21:24:15 +0200

    cleanup

--- src/components/molecules/ErrorWindow/ErrorWindow.stories.ts
+++ src/components/molecules/ErrorWindow/ErrorWindow.stories.ts
@@ -1,8 +1,6 @@
 import type {Meta, StoryObj} from "@storybook/react";
 import ErrorWindow from "./ErrorWindow.tsx";
 
-// TODO - not a molecule? (At least not yet)
-
 const meta = {
     title: "Molecules/Error Window",
     component: ErrorWindow,
